57fa239aa8d52b15769efa9345520e4546addfb7
Keval <kevalradadiya72020@gmail.com>
2023-06-01 08:33:29 +0530

    routes security removed

--- client/src/components/app-routes.jsx
+++ client/src/components/app-routes.jsx
@@ -23,7 +23,8 @@ const AppRoutes = () => (
         // ) : (
         //   <Route key={itm.path} element={itm.element} path={itm.path} />
         // );
-        return itm.adminOnly ? isAdmin() && <Route key={itm.path} element={itm.element} path={itm.path} /> : <Route key={itm.path} element={itm.element} path={itm.path} />
+        // return itm.adminOnly ? isAdmin() && <Route key={itm.path} element={itm.element} path={itm.path} /> : <Route key={itm.path} element={itm.element} path={itm.path} />
+        <Route key={itm.path} element={itm.element} path={itm.path} />
       })}
     </Route>
 
